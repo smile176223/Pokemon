9eda4716673925acf1c4b4ec6f7e0d8e4bbbb2b1
LiamHsu <s459958@gmail.com>
2024-09-03 09:17:54 +0800

    Add test target

---
+++ PokemonTests/PokemonTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonTests.swift
+//  PokemonTests
+//
+//  Created by Liam on 2024/9/3.
+//
+
+import XCTest
+
+final class PokemonTests: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
