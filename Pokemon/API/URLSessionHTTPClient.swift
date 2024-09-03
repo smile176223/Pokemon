a8267ee7d182dfe4a9eca10b731cce1b0ab2a374
LiamHsu <s459958@gmail.com>
2024-09-03 14:31:08 +0800

    Create URLProtocolStub

---
+++ Pokemon/API/URLSessionHTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  URLSessionHTTPClient.swift
+//  Pokemon
+//
+//  Created by Liam on 2024/9/3.
+//
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        
+    }
+}
---
+++ PokemonTests/API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,12 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  PokemonTests
+//
+//  Created by Liam on 2024/9/3.
+//
+
+import XCTest
+
+final class URLSessionHTTPClientTests: XCTestCase {
+    
+}
