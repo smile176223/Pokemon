c48e59a7c6e2d13dcfcc0a71658f92ec77e85be9
LiamHsu <s459958@gmail.com>
2024-09-03 11:19:26 +0800

    Extract expect method to reduce duplicate code

--- PokemonTests/LoadPokemonFromRemoteUseCaseTests.swift
+++ PokemonTests/LoadPokemonFromRemoteUseCaseTests.swift
@@ -55,22 +55,9 @@ final class LoadPokemonFromRemoteUseCaseTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        let exp = expectation(description: "Wait for completion")
-        sut.load { receivedResult in
-            switch receivedResult {
-            case .success:
-                XCTFail("Should get error")
-                
-            case let .failure(error):
-                XCTAssertEqual(error, .unexpectedError)
-            }
-            
-            exp.fulfill()
-        }
-        
-        client.complete(with: NSError(domain: "Test", code: 0))
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toCompleteWith: .failure(.unexpectedError), when: {
+            client.complete(with: anyError())
+        })
     }
     
     func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
@@ -79,22 +66,9 @@ final class LoadPokemonFromRemoteUseCaseTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
 
         samples.enumerated().forEach { index, sample in
-            let exp = expectation(description: "Wait for completion")
-            sut.load { receivedResult in
-                switch receivedResult {
-                case .success:
-                    XCTFail("Should get error")
-                    
-                case let .failure(error):
-                    XCTAssertEqual(error, .invalidData)
-                }
-                
-                exp.fulfill()
-            }
-            
-            client.complete(withStatusCode: sample, data: anyData(), at: index)
-            
-            wait(for: [exp], timeout: 1.0)
+            expect(sut, toCompleteWith: .failure(.invalidData), when: {
+                client.complete(withStatusCode: sample, data: anyData(), at: index)
+            })
         }
     }
     
@@ -109,12 +83,45 @@ final class LoadPokemonFromRemoteUseCaseTests: XCTestCase {
         return (loader, client)
     }
     
+    private func expect(
+        _ sut: RemotePokemonLoader,
+        toCompleteWith expectedResult: Result<String, RemotePokemonLoader.Error>,
+        when action: () -> Void,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     private func anyURL() -> URL {
         URL(string: "https://any-url.com")!
     }
     
     private func anyData() -> Data {
         Data("any".utf8)
+    }
+    
+    private func anyError() -> Error {
+        NSError(domain: "any error", code: 0)
     }
 
 }
