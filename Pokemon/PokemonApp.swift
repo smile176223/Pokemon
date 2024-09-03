668442c0514bd2ed99c040125cecf0584747395b
LiamHsu <s459958@gmail.com>
2024-09-03 08:57:39 +0800

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Liam on 2024/9/3.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
