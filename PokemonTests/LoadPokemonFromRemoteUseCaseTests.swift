dccd1de28d7387c70e686bfcbcf6f7b6c911f8b3
LiamHsu <s459958@gmail.com>
2024-09-03 10:51:58 +0800

    Guarantee load delivers error on client error

--- PokemonTests/LoadPokemonFromRemoteUseCaseTests.swift
+++ PokemonTests/LoadPokemonFromRemoteUseCaseTests.swift
@@ -14,12 +14,22 @@ public protocol HTTPClient {
 }
 
 public final class RemotePokemonLoader {
-    let url: URL
-    let client: HTTPClient
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case unexpectedError
+    }
     
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
+    }
+    
+    public func load(completion: @escaping (Result<String, Error>) -> Void) {
+        client.get(from: url) { result in
+            completion(.failure(Error.unexpectedError))
+        }
     }
 }
 
@@ -30,8 +40,16 @@ class HTTPClientSpy: HTTPClient {
         return messages.map { $0.url }
     }
     
-    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), any Error>) -> Void) {
+    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
         messages.append((url, completion))
+    }
+    
+    func complete(with error: Error, at index: Int = 0, file: StaticString = #file, line: UInt = #line) {
+        guard messages.count > index else {
+            return XCTFail("Can't complete request never made", file: file, line: line)
+        }
+        
+        messages[index].completion(.failure(error))
     }
 }
 
@@ -43,6 +61,29 @@ final class LoadPokemonFromRemoteUseCaseTests: XCTestCase {
         let loader = RemotePokemonLoader(url: url, client: client)
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let loader = RemotePokemonLoader(url: url, client: client)
+        
+        let exp = expectation(description: "Wait for completion")
+        loader.load { receivedResult in
+            switch receivedResult {
+            case let .success(item):
+                XCTFail("Should get error")
+                
+            case let .failure(error):
+                XCTAssertEqual(error, RemotePokemonLoader.Error.unexpectedError)
+            }
+            
+            exp.fulfill()
+        }
+        
+        client.complete(with:  NSError(domain: "Test", code: 0))
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Helper
